96eb220e307a718c0f9e4f7b8f2e8ba9c6d6eda4
Nick Flores <nick.flores@whiz.pe>
2022-03-21 01:13:09 -0500

    adding footer, header and some dirs

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+    return (
+      <footer>
+        <div className="footerContainer">
+            <div className="footerInfo">
+
+            </div>
+            <div className="footerSocialMedia">
+
+            </div>
+        </div>
+      </footer>
+    );
+  }
+  
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
+    
     <App />
+
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
