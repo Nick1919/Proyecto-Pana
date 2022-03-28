2e0a62dd4fcb39253af0a51be29657dacef2ff5b
Nick Flores <nick.flores@whiz.pe>
2022-03-27 21:02:43 -0500

    changes on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,26 @@ import React from "react";
 function Footer() {
     return (
       <footer>
-        <div className="footerContainer">
-            <div className="footerInfo">
-                
-            </div>
-            <div className="footerSocialMedia">
+        <div className="container">
+          <div className="col">
 
+            <div className="footerContainer">
+              <div className="footerInfo">
+                  
+              </div>
+
+              <div className="copyrightContainer">
+                  <span className="footerLogo"></span>
+
+                  <span className="">© 2022 All rights reserved - Powered by LOSPANAS</span>
+              </div>
+
+              <div className="footerSocialMedia">
+
+              </div>
             </div>
+
+          </div>
         </div>
       </footer>
     );
