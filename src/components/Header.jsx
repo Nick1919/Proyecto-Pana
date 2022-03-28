28c6a43853a4bd464a325fffca3927d04c3a43f7
Nick Flores <nick.flores@whiz.pe>
2022-03-27 20:51:52 -0500

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,24 @@ function Header() {
     return (
       <header>
         <div className="container">
-          <div className="col"></div>
-            <div className="headerMenuContainer">
-                <div className="headerMenu">
-                    
-                </div>
+          <div className="col">
+              <div className="headerMenuContainer">
+                  <div className="headerMenu">
+                      <span className="headerMenuIcon"></span>
+                  </div>
 
-                <div className="headerLogoContainer">
-                  <span className="headerLogo"></span>
-                </div>
+                  <div className="headerLogoContainer">
+                    <span className="headerLogo"></span>
+                  </div>
 
-                <nav className="headerNav">
-                    <ul>
-                        <li className="listemItem1">Conocenos</li>
-                        <li className="listemItem2">Registrate</li>
-                        <li className="listemItem3">Contribuir??</li>
-                    </ul>
-                </nav>
+                  <nav className="headerNav">
+                      <ul>
+                          <li className="listemItem1">Conócenos</li>
+                          <li className="listemItem2">Registrate</li>
+                          <li className="listemItem3">Contribuir</li>
+                      </ul>
+                  </nav>
+              </div>
             </div>
           </div>
       </header>
