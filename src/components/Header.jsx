62a3e4cb86e4da4cbd887237c713a4bcfbe7f4b0
Nick Flores <nick.flores@whiz.pe>
2022-03-27 20:04:42 -0500

    adding flexbox-grid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,26 @@ import React from "react";
 function Header() {
     return (
       <header>
-        <div className="headerMenuContainer">
-            <div className="headerMenu">
+        <div className="container">
+          <div className="col"></div>
+            <div className="headerMenuContainer">
+                <div className="headerMenu">
+                    
+                </div>
 
+                <div className="headerLogoContainer">
+                  <span className="headerLogo"></span>
+                </div>
+
+                <nav className="headerNav">
+                    <ul>
+                        <li className="listemItem1">Conocenos</li>
+                        <li className="listemItem2">Registrate</li>
+                        <li className="listemItem3">Contribuir??</li>
+                    </ul>
+                </nav>
             </div>
-            <nav className="headerNav">
-                <ul>
-                    <li className="listemItem1">Conocenos</li>
-                    <li className="listemItem2">Registrate</li>
-                    <li className="listemItem3">Contribuir??</li>
-                </ul>
-            </nav>
-        </div>
+          </div>
       </header>
     );
   }
